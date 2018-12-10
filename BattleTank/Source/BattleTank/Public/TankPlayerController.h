3a0ef2bc881c58c0621785f24d4656a44e0334c5
Mimikyu <imlauraok@gmail.com>
2018-12-10 13:02:59 +0000

    Using Blueprint Variables

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -16,15 +16,16 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
-	
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
 
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+	ATank* GetControlledTank() const;
+
 private:
-	// Start the tank moving the barrel so that a shot would hit where
-	// the crosshair intersects the world
+	// Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
 	void AimTowardsCrosshair();
 
 	// Returns an OUT parameter, true if hit landscape
